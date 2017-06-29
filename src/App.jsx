b80835c65431b284611bad0d89381add51a6cd3e
otto hu <ottohu101@gmail.com>
2017-06-29 00:25:50 +0000

    new feature message send to the server and server broadcast to all clients

--- src/App.jsx
+++ src/App.jsx
@@ -6,47 +6,35 @@ import NavBar from './NavBar.jsx';
 
 class App extends Component {
 
-    // in App.jsx
-  componentDidMount() {
-    console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
-  }
-
-  addNewMessage(bundle){
-    const newMessage = {id:this.state.messages.length+1, username: bundle.name, content: bundle.content}; 
-    const messages = this.state.messages.concat(newMessage);
-    this.setState({currentUser: {name: bundle.name},
-      messages: messages
-    });
-  }
-
   constructor(props){
     super(props);
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          id: 1,
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: 2,
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: []
     };
-    this.addNewMessage = this.addNewMessage.bind(this);
   }
+    // in App.jsx
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+
+    this.ws = new WebSocket("ws://localhost:3001");
+    this.ws.onmessage = (message)=>{
+      console.log(JSON.parse(message.data));
+      const msg = JSON.parse(message.data);
+      const messages = this.state.messages.concat(msg);
+      this.setState({messages});
+    }
+    this.ws.onopen = (event) => {
+      console.log("connected");
+    }
+  }
+
+  addNewMessage(bundle){
+    const newMessage = {type:"sendMessage", username: bundle.name, content: bundle.content}; 
+    const messages = this.state.messages.concat(newMessage);
+    this.ws.send(JSON.stringify(newMessage));
+  }
+
 
  
 
@@ -59,7 +47,7 @@ class App extends Component {
       <div className="message system">
         Anonymous1 changed their name to nomnom.
       </div>
-      <ChatBar addNewMessage = {this.addNewMessage} currentUser = {this.state.currentUser} />
+      <ChatBar addNewMessage = {this.addNewMessage.bind(this)} currentUser = {this.state.currentUser} />
       </div>
       
     );
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,7 +4,7 @@ class ChatBar extends Component {
   constructor(props){
     super(props);
     this.state = {
-      name : this.props.name,
+      name : this.props.currentUser.name,
       content : ""
     };
     this.messageHandler = this.messageHandler.bind(this);
@@ -21,14 +21,14 @@ class ChatBar extends Component {
     }
   }
   setName(e){
-    this.setState({name: e.target.value||"Anonymous"});
+    this.setState({name: e.target.value});
   }
   render() {
     console.log('Rendering <ChatBar />');
     return (
       <div>
         <footer className="chatbar" >
-          <input className="chatbar-username" placeholder={this.props.currentUser.name} onChange = {this.setName} defaultValue = {this.state.name}/>
+          <input className="chatbar-username" placeholder={this.props.currentUser.name} onChange = {this.setName} value = {this.state.name}/>
           <input className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress = {this.enterHandler} onChange = {this.messageHandler} value = {this.state.content}/>
         </footer>
       </div>
